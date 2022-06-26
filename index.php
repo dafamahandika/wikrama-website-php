3f6db65de0dc5676e2ab88ce062e5196685a6cc7
dafamahandika <dafamahandika@gmail.com>
2022-06-26 15:05:14 +0700

    edit css card dan footer

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
       <a href="index.php" class="brand"><b>SMK Wikrama Bogor</b></a>
       <nav>
         <ul class="navbar">
-          <li><a href="page=sejarah">Sejarah</a></li>
-          <li><a href="page=kompetensi">Kompetensi</a></li>
-          <li><a href="page=matrikulasi">Matrikulasi</a></li>
-          <li><a href="page=kantin">Kantin</a></li>
+          <li><a href="index.php?page=sejarah">Sejarah</a></li>
+          <li><a href="index.php?page=kompetensi">Kompetensi</a></li>
+          <li><a href="index.php?page=matrikulasi">Matrikulasi</a></li>
+          <li><a href="index.php?page=kantin">Kantin</a></li>
         </ul>
       </nav>
     </header>
@@ -36,9 +36,6 @@
         </p>
       </div>
     </div>
-    <?php
-require 'function.php'; 
-?>
     <div class="qoute">
       <div class="komitmen">
         <h4>Komitmen</h4>
@@ -61,5 +58,37 @@ require 'function.php';
         <p><i>Padamu negeri kami berjanji lulus Wikrama siap membangun negeri</i></p>
       </div>
     </div>
+
+    <footer>
+    <div class="footer-bagian">
+
+        <div class="foto-wikrama">
+            <img src="img/logo.png" alt="">
+        </div>
+
+        <div class="list-halaman">
+            <h2>Tentang Wikrama</h2>
+            <ul>
+                <li><a href="">Sejarah</a></li>
+                <li><a href="">Kompetensi</a></li>
+                <li><a href="">Matrikulasi </a></li>
+                <li><a href="">Kantin</a></li>
+            </ul>
+        </div>
+
+        <div class="kontak">
+            <div class="alamat">
+                <h2>Alamat Sekolah</h2>
+                <h3>JL. RAYA WANGUN KEL. SINDANGSARI BOGOR, Kota Bogor, Jawa Barat</h3>
+            </div>
+            <div class="telepon">
+                <h2>Kontak</h2>
+                <h3>0251-8242411 <br>
+                    082221718035 (Whatsapp)</h3>
+            </div>
+        </div>
+        
+    </div>
+</footer>
   </body>
 </html>
